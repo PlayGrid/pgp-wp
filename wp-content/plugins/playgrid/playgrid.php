5fd67764dcef0113cc2a49414e58a595e75ee5a4
Jens Page <jens.page@datajockey.org>
2013-09-17 16:48:42 -0400

    setting up the playgrid plugin project

---
+++ wp-content/plugins/playgrid/playgrid.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: PlayGrid
+ * Plugin URI: http://www.playgrid.com/wordpress_plugin
+ * Description: Integrate your Minecraft game site with PlayGrid.
+ * Version: 0.1
+ * Author: PlayGrid
+ * Author URI: http://www.playgrid.com
+ * License: GPL2
+ */
+
+
+
+
+
